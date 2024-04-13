204bebd8e4f06dbd42fcc1466705a138d0e9ed41
sideeq12 <abdwaheed2018@gmail.com>
2024-04-13 18:09:17 +0100

    footer

--- packages/blog-starter-kit/themes/enterprise/components/header.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/header.tsx
@@ -52,7 +52,9 @@ export function Header() {
 			alt="ABC Foundation Logo"
 		  />
 		  <div className="ml-auto hidden items-center gap-14 lg:flex text-2xl">
-			{links.map((link) => ( (link.name == "Resources" ) ? <Custom   key={30}/>
+			{links.map((link) => ( (link.name == "Resources" ) ?
+			//  <Custom   key={30}/> 
+			null
 			:  (<Link key={link.name} href={link.href}
 			  className={clsx({
 				"bg-sky-100 text-abcf": pathname === link.href,
@@ -99,7 +101,7 @@ export function Header() {
 				  About
 				</Link>
 			  </li>
-			 <Custom />
+			 {/* <Custom /> */}
 			  <li>
 				<Link  href="/main/contacts">
 				  Contacts
@@ -124,88 +126,88 @@ export function Header() {
   }
   
   
-interface DropMenuProps {
-	title: string;
-	items: {name: string; href : string}[];
-  }
+// interface DropMenuProps {
+// 	title: string;
+// 	items: {name: string; href : string}[];
+//   }
   
   
-  const DropMenu: React.FC<DropMenuProps & { isOpen: boolean; toggleDropdown: () => void }> = ({ title, items, isOpen, toggleDropdown }) => {
-	return (
-	  <div className="relative flex flex-col md:flex-row text-left">
-		<div
-		  onClick={toggleDropdown}
-		  className="inline-flex justify-start w-full md:justify-center md:px-4 py-2 bg-white  font-medium"
-		  aria-haspopup="true"
-		  aria-expanded={isOpen}
-		>
-		  {title}
-		  {isOpen ? <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-up  ml-3 mt-2" viewBox="0 0 16 16">
-	<path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z"/>
-  </svg> :<svg
-			className="-mr-1 ml-2 mt-2 h-5 w-5"
-			xmlns="http://www.w3.org/2000/svg"
-			viewBox="0 0 20 20"
-			fill="currentColor"
-			aria-hidden="true"
-		  >
-			<path
-			  fillRule="evenodd"
-			  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-			  clipRule="evenodd"
-			/>
-		  </svg>}
-		</div>
+//   const DropMenu: React.FC<DropMenuProps & { isOpen: boolean; toggleDropdown: () => void }> = ({ title, items, isOpen, toggleDropdown }) => {
+// 	return (
+// 	  <div className="relative flex flex-col md:flex-row text-left">
+// 		<div
+// 		  onClick={toggleDropdown}
+// 		  className="inline-flex justify-start w-full md:justify-center md:px-4 py-2 bg-white  font-medium"
+// 		  aria-haspopup="true"
+// 		  aria-expanded={isOpen}
+// 		>
+// 		  {title}
+// 		  {isOpen ? <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-up  ml-3 mt-2" viewBox="0 0 16 16">
+// 	<path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z"/>
+//   </svg> :<svg
+// 			className="-mr-1 ml-2 mt-2 h-5 w-5"
+// 			xmlns="http://www.w3.org/2000/svg"
+// 			viewBox="0 0 20 20"
+// 			fill="currentColor"
+// 			aria-hidden="true"
+// 		  >
+// 			<path
+// 			  fillRule="evenodd"
+// 			  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+// 			  clipRule="evenodd"
+// 			/>
+// 		  </svg>}
+// 		</div>
   
-		<div
-		  className={`absolute z-10  w-56 rounded-md  mt-8 md:mt-12 shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 ${
-			isOpen ? '' : 'hidden'
-		  }`}
-		  role="menu"
-		  aria-orientation="vertical"
-		  aria-labelledby="options-menu"
-		>
-		  <div className="py-1" role="none">
-			{items.map((item, index) => (
-			  <a
-				key={index}
-				href={item.href}
-				className="text-gray-700 block px-4 py-2 text-base"
-				role="menuitem"
-			  >
-				{item.name}
-			  </a>
-			))}
-		  </div>
-		</div>
-	  </div>
-	);
-  };
+// 		<div
+// 		  className={`absolute z-10  w-56 rounded-md  mt-8 md:mt-12 shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 ${
+// 			isOpen ? '' : 'hidden'
+// 		  }`}
+// 		  role="menu"
+// 		  aria-orientation="vertical"
+// 		  aria-labelledby="options-menu"
+// 		>
+// 		  <div className="py-1" role="none">
+// 			{items.map((item, index) => (
+// 			  <a
+// 				key={index}
+// 				href={item.href}
+// 				className="text-gray-700 block px-4 py-2 text-base"
+// 				role="menuitem"
+// 			  >
+// 				{item.name}
+// 			  </a>
+// 			))}
+// 		  </div>
+// 		</div>
+// 	  </div>
+// 	);
+//   };
   
-  const Custom: React.FC = () => {
+//   const Custom = () => {
   
-	  const [openDropdown, setOpenDropdown] = useState<number | null>(null);
-	const toggleDropdown = (dropdownId: number) => {
-	  setOpenDropdown(openDropdown === dropdownId ? null : dropdownId);
-	};
+// 	  const [openDropdown, setOpenDropdown] = useState<number | null>(null);
+// 	const toggleDropdown = (dropdownId: number) => {
+// 	  setOpenDropdown(openDropdown === dropdownId ? null : dropdownId);
+// 	};
   
-	return (
-	  <div className="sm:flex sm:justify-center gap-4 sm:items-center">
-		<DropMenu
-		  title="Resources"
-		  items={store[0].submenuItems}
-		  isOpen={openDropdown === 1}
-		  toggleDropdown={() => toggleDropdown(1)}
-		/>
-		<DropMenu
-		  title="Initiatives"
-		  items={store[1].submenuItems}
-		  isOpen={openDropdown === 2}
-		  toggleDropdown={() => toggleDropdown(2)}
-		/>
-	  </div>
-	);
-  };
+// 	return (
+// 	  <div className="sm:flex sm:justify-center gap-4 sm:items-center">
+// 		<DropMenu
+// 		  title="Resources"
+// 		  items={store[0].submenuItems}
+// 		  isOpen={openDropdown === 1}
+// 		  toggleDropdown={() => toggleDropdown(1)}
+// 		/>
+// 		<DropMenu
+// 		  title="Initiatives"
+// 		  items={store[1].submenuItems}
+// 		  isOpen={openDropdown === 2}
+// 		  toggleDropdown={() => toggleDropdown(2)}
+// 		/>
+// 	  </div>
+// 	);
+//   };
   
 
   
